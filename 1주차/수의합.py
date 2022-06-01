4b5a388d1532bc82f1a309e2d4123b67c871a082
somm12 <sominan8744@gmail.com>
2022-06-01 16:15:40 +0900

    수의합.py update

--- 1주차/수의합.py
+++ 1주차/수의합.py
@@ -1,7 +1,7 @@
 n, m = map(int,input().split())
 
 arr = list(map(int,input().split()))
-# left 인덱스 부터 right 인덱스 전! 까지의 합이 m과 같은 경우를 구한다.
+
 left = 0
 right = 1
 total = arr[0]
@@ -22,3 +22,6 @@ while True:
         total -= arr[left]
         left += 1
 print(count)
+
+# left 인덱스 부터 right 인덱스 전! 까지의 합이 m과 같은 경우를 구한다.
+# 만일 합이 m과 같거나 m보다 커진다면 left를 한 칸 옮겨야 하므로 전까지 더했던 total에서 arr[left]를 빼줘야한다.
