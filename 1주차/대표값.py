1600d15f78d6a4faffdc9764ead0d5e632767968
“somm12” <sominan8744@gmail.com>
2022-06-01 12:09:35 +0900

    카드역배치.py update

--- 1주차/대표값.py
+++ 1주차/대표값.py
@@ -1,11 +1,10 @@
 n = int(input())
 arr = list(map(int,input().split()))
-# round 사용시 조심: round(4.5) => 4 출력 됨.
+
 average = (sum(arr)/n) + 0.5
 average = int(average)
 min = 21470000
 
-#enumerate : 배열의 index와 값을 받아오는 내장함수.
 
 for idex, value in enumerate(arr):
     temp = abs(average - value)
@@ -17,4 +16,6 @@ for idex, value in enumerate(arr):
         if value > score:
             score = value
             number = idex+1
+# round 사용시 조심: round(4.5) => 4 출력 됨.
+# enumerate : 배열의 index와 값을 받아오는 내장함수.
 #몇 번째 인지 알아야하는데, index를 사용했으니 idex + 1을 해야한다.
