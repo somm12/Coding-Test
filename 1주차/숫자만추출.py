ca15ab1f3a67294502dda55abdfeb8c3c6a70a6f
“somm12” <sominan8744@gmail.com>
2022-06-01 11:42:20 +0900

    숫자만추출.py update

---
+++ 1주차/숫자만추출.py
@@ -0,0 +1,16 @@
+arr = input()
+answer = ""
+for i in arr:
+    if i.isnumeric():
+        answer += i
+answer = int(answer)
+print(answer)
+
+#약수의 개수
+numOfDivisor = 0
+for i in range(1, answer+ 1):
+    if answer % i == 0:
+        numOfDivisor += 1
+print(numOfDivisor)
+
+# 숫자인지 아닌지 판별 isnumeric, isdigit, isdecimal
